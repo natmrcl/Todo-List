d656ce5c12d7f5b90b178a1afe836360edcf8322
natmrcl <natyamm10@gmail.com>
2023-05-22 23:35:50 +0700

    update

--- src/component/TodoForm/TodoForm.js
+++ src/component/TodoForm/TodoForm.js
@@ -1,4 +1,6 @@
 import React,{useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './Form.css'
 
 export const TodoForm = ({addTodo}) => {
@@ -20,6 +22,10 @@ export const TodoForm = ({addTodo}) => {
       }
     };
 
+    const handleAddButton = () => {
+      handleSubmit(new Event('submit'));
+    };
+
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
       <div className='title'>Add New Task</div>
@@ -27,7 +33,13 @@ export const TodoForm = ({addTodo}) => {
         placeholder='What is the task today?' onChange={(e) => setTask(e.target.value)}/>  
         <textarea className="todo-description" value={description}
         placeholder="Add a description..."
-        onChange={e => setDescription(e.target.value)} onKeyDown={handleKeyDown}></textarea>
+        onChange={e => setDescription(e.target.value)} onKeyDown={handleKeyDown}>
+        </textarea>
+        <div className='icon'>
+          <button type='button' className='add-button' onClick={handleAddButton}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+        </div>
     </form>
 
   )
